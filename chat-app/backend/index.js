49a1348daada27c647d088bced10c6af569d51f3
Alex <oleksi.ovsiannikov@gmail.com>
2026-05-19 02:18:31 +0200

    Create chat application

---
+++ chat-app/backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+const messages = [];
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "index.html")
+  );
+});
+
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.post("/messages", (req, res) => {
+  const newMessage = {
+    text: req.body.text,
+  };
+
+  messages.push(newMessage);
+
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
